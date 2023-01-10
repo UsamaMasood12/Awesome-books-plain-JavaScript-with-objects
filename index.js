0132fa6d3dbbe0c43504924092d038d919ef915d
mrprotocoll <lekanvgbg@gmail.com>
2023-01-10 09:10:12 +0100

    Add script to use class functions

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ const navListBtn = document.querySelector('#nav-list');
 const navAddNewBtn = document.querySelector('#nav-add-new');
 const navContactBtn = document.querySelector('#nav-contact');
 
+if (!JSON.parse(localStorage.getItem('books'))) {
+  localStorage.setItem('books', JSON.stringify([]));
+}
+
+// Show the books on localStorage
+BookManagement.display();
+// Add a book
+addForm.addEventListener('submit', addNew);
+// Dynamic remove buttons
+const deleteButtons = document.querySelectorAll('.delete-button');
+deleteButtons.forEach(removeBooks);
+// Time
+const time = document.querySelector('#time');
+const dt = DateTime.now();
+time.innerHTML = dt.toLocaleString(DateTime.DATETIME_MED);
+// Navigation
+navListBtn.addEventListener('click', showListSection);
+navAddNewBtn.addEventListener('click', showAddNew);
+navContactBtn.addEventListener('click', showContact);
