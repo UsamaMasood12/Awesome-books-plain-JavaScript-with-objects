fcf5ab209b0c7d6cedc734e61e57fd9e0bb584a9
Usama Masood <umasood.bee17seecs@seecs.edu.pk>
2023-01-10 12:53:44 +0500

    Creation of list using classes

--- index.js
+++ index.js
@@ -1,48 +1,23 @@
-const savedBooks = localStorage.getItem('save');
-let ourBooks = [];
-if (savedBooks) {
-  ourBooks = JSON.parse(savedBooks);
-} else {
-  const demoData = [
-    {
-      id: 1,
-      name: 'Book1',
-      author: 'Author1',
-    },
-    {
-      id: 2,
-      name: 'Book2',
-      author: 'Author2',
-    },
-  ];
-  localStorage.setItem('save', JSON.stringify(demoData));
-  ourBooks = JSON.parse(savedBooks);
-  // eslint-disable-next-line no-restricted-globals
-  location.reload();
-}
-// eslint-disable-next-line no-unused-vars
-function removeBook(id) {
-  ourBooks = ourBooks.filter((book) => {
-    if (id === book.id) {
-      return false;
-    }
-    return true;
-  });
-  localStorage.setItem('save', JSON.stringify(ourBooks));
-  // eslint-disable-next-line no-restricted-globals
-  location.reload();
-}
+import
+BookManagement
+from './modules/bookmanagement.js';
+import
+addNew
+from './modules/addNew.js';
+import
+removeBooks
+from './modules/removeBooks.js';
+import {
+  showListSection,
+  showAddNew,
+  showContact,
+} from './modules/navigation.js';
+import {
+  DateTime,
+} from './modules/luxon.js';
 
-const bookList = document.querySelector('#book-list');
-ourBooks.forEach((book) => {
-  const singleBook = document.createElement('div');
-  singleBook.classList.add('single-book');
-  singleBook.innerHTML = ` 
-  <p>${book.name}</p>
-  <p>${book.author}</p>
-  <button onclick=removeBook(${book.id}) >Remove</button>
-  <hr>
-  `;
-  bookList.appendChild(singleBook);
-});
+const addForm = document.querySelector('form');
+const navListBtn = document.querySelector('#nav-list');
+const navAddNewBtn = document.querySelector('#nav-add-new');
+const navContactBtn = document.querySelector('#nav-contact');
 
